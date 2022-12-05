845247d8b79cce27de1f765316781266622f9f35
PANKAJ <PANKAJ@DESKTOP-LV1N0V2>
2022-12-05 17:20:02 +0530

    [nayana]:create sample2 class

---
+++ src/adcdc/sample1.java
@@ -0,0 +1,9 @@
+package adcdc;
+
+public class sample1 
+{
+public static void main(String[] args) 
+{
+System.out.println("ok thanks");	
+}
+}
---
+++ src/adcdc/sample2.java
@@ -0,0 +1,8 @@
+package adcdc;
+
+public class sample2 
+{
+public static void main(String[] args) {
+	System.out.println("hi hello");
+}
+}
